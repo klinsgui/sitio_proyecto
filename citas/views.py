2ca7d67d35ca450116e0c3ab11804056536201aa
= <klinsmanm95@mesoamericana.edu.gt>
2015-11-12 01:05:37 -0600

    se modificó la condición de la fecha

--- citas/views.py
+++ citas/views.py
@@ -4,5 +4,5 @@ from django.utils import timezone
 
 # Create your views here.
 def listar_citas(request):
-    citasprogramadas = Citar.objects.filter(realizada=False, fecha_cita=timezone.now())#.order_by('fecha_cita'))
+    citasprogramadas = Citar.objects.filter(realizada=False, fecha_cita__lte=timezone.now())#.order_by('fecha_cita'))
     return render(request, 'citas/listar.html', {'citas': citasprogramadas})
