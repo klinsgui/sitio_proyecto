c6a666c89e051b2c6ed706c3625afc1aa09507d2
= <klinsmanm95@mesoamericana.edu.gt>
2015-11-12 00:17:17 -0600

    Se modificó el archivo listar.html para que mostrara exactamente las citas almacenadas en el modelo

--- citas/views.py
+++ citas/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import Citar
+from django.utils import timezone
 
 # Create your views here.
 def listar_citas(request):
-    return render(request, 'citas/listar.html', {})
+    citasprogramadas = Citar.objects.filter(fecha_cita__lte=timezone.now()).order_by('fecha_cita')
+    return render(request, 'citas/listar.html', {'citas': citasprogramadas})
