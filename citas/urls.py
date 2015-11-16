2b41efd70c2233ed01623ecb72c6b21860af271d
= <klinsmanm95@mesoamericana.edu.gt>
2015-11-16 08:19:18 -0600

    Se incorporó la plantilla para detalle de cita, junto con la vista de detalle para ampliar la aplicación

--- citas/urls.py
+++ citas/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.listar_citas),
+    url (r'^cita/(?P<pk>[0-9]+)/$', views.detalle_cita),
 ]
--- citas/views.py
+++ citas/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Citar
 from django.utils import timezone
 
@@ -6,3 +6,7 @@ from django.utils import timezone
 def listar_citas(request):
     citasprogramadas = Citar.objects.filter(realizada=False, fecha_cita__lte=timezone.now())#.order_by('fecha_cita'))
     return render(request, 'citas/listar.html', {'citas': citasprogramadas})
+
+def detalle_cita(request, pk):
+    cit = get_object_or_404(Citar, pk=pk)
+    return render(request, 'citas/detalle.html', {'cita': cit})
